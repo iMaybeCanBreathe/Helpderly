e227ca1788b2b998cd4e0129736920c530e58b03
Yu Min Tan <s10195143@connect.np.edu.sg>
2020-11-06 09:39:24 +0800

    Add project files.

---
+++ FSD_Helpderly/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FSD_Helpderly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
