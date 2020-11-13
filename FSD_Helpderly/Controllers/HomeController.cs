cce31fee53d205eae6c9baf8040a87f30beb93c7
Rafael Kamil <s10192797@connect.np.edu.sg>
2020-11-13 10:19:37 +0800

    Sycn with yumin Updates

--- FSD_Helpderly/Controllers/HomeController.cs
+++ FSD_Helpderly/Controllers/HomeController.cs
@@ -16,16 +16,15 @@ namespace FSD_Helpderly.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
         public IActionResult VolunteerViewPost()
         {
             return View();
