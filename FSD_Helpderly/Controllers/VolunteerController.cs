5980fd86f96b994ccbeaf70226d5ac52f3eab504
Yu Min Tan <s10195143@connect.np.edu.sg>
2020-11-18 17:38:11 +0800

    created volunteer controller

---
+++ FSD_Helpderly/Controllers/VolunteerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FSD_Helpderly.DAL;
+using FSD_Helpderly.Models;
+
+namespace FSD_Helpderly.Controllers
+{
+    public class VolunteerController : Controller
+    {
+        private FirestoreDAL fDal = new FirestoreDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        async public Task<IActionResult> SelectedViewPost()
+        {
+            List<object> SelectedFormId = await fDal.GetVolunteerForms("BBean@yahoo.com");
+            List<ElderlyPost> selectedform = new List<ElderlyPost>();
+            foreach (string formid in SelectedFormId)
+            {
+                ElderlyPost form = await fDal.GetForm(formid);
+                selectedform.Add(form);
+            }
+
+            return View("../Volunteers/SelectedViewPost", selectedform);
+        }
+    }
+}
