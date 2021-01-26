482c7bc82ded733b694d3eb5cae1981f4028fe32
Yu Min Tan <s10195143@connect.np.edu.sg>
2021-01-26 15:44:54 +0800

    Added: - Elderly OTP Login - Eldelry View Requests Page - Elderly Autofill Data - Volunteer Update Status - Volunteer View Ongoing/Done Requests

--- FSD_Helpderly/Controllers/VolunteerController.cs
+++ FSD_Helpderly/Controllers/VolunteerController.cs
@@ -18,7 +18,7 @@ namespace FSD_Helpderly.Controllers
             return RedirectToAction("ViewAllPosts", "Home");
         }
 
-        async public Task<IActionResult> SelectedViewPost()
+        async public Task<IActionResult> ViewActivePosts()
         {
             if (HttpContext.Session.GetString("Role") != "Volunteer")
             {
@@ -31,10 +31,35 @@ namespace FSD_Helpderly.Controllers
             foreach (string formid in selectedFormIds)
             {
                 ElderlyPost form = await fDal.GetForm(formid);
-                selectedForms.Add(form);
+                if (form.Status == "ongoing")
+                {
+                    selectedForms.Add(form);
+                }
             }
 
             return View("../Volunteers/SelectedViewPost", selectedForms);
+        }
+
+        async public Task<IActionResult> ViewDonePosts()
+        {
+            if (HttpContext.Session.GetString("Role") != "Volunteer")
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            string email = HttpContext.Session.GetString("Email");
+            List<object> selectedFormIds = await fDal.GetVolunteerForms(email);
+            List<ElderlyPost> selectedForms = new List<ElderlyPost>();
+            foreach (string formid in selectedFormIds)
+            {
+                ElderlyPost form = await fDal.GetForm(formid);
+                if (form.Status == "done")
+                {
+                    selectedForms.Add(form);
+                }
+            }
+
+            return View("../Volunteers/ViewDonePost", selectedForms);
         }
 
         public IActionResult AcceptPost(string formId)
@@ -46,7 +71,7 @@ namespace FSD_Helpderly.Controllers
 
             string email = HttpContext.Session.GetString("Email");
             fDal.VolunteerAcceptForm(email, formId);
-            return RedirectToAction("SelectedViewPost");
+            return RedirectToAction("ViewActivePosts");
             
         }
 
@@ -59,7 +84,29 @@ namespace FSD_Helpderly.Controllers
 
             string email = HttpContext.Session.GetString("Email");
             fDal.VolunteerCancelForm(email, formId);
-            return RedirectToAction("SelectedViewPost");
+            return RedirectToAction("ViewActivePosts");
+        }
+
+        public IActionResult SetPostDone(string formId)
+        {
+            if (HttpContext.Session.GetString("Role") != "Volunteer")
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            fDal.UpdateFormStatus(formId, true);
+            return RedirectToAction("ViewActivePosts");
+        }
+
+        public IActionResult SetPostOngoing(string formId)
+        {
+            if (HttpContext.Session.GetString("Role") != "Volunteer")
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            fDal.UpdateFormStatus(formId, false);
+            return RedirectToAction("ViewDonePosts");
         }
     }
 }
